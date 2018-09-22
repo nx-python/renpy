9c2e86251c6b09df1ace41f6f785e03ab5cc2d82
noah the goodra <peterpan0413@live.com>
2018-09-21 23:12:13 -0500

    add more fixes to list
    Signed-off-by: noah the goodra <peterpan0413@live.com>

--- renpy/py2to3ports.py
+++ renpy/py2to3ports.py
@@ -1,4 +1,5 @@
 from lib2to3.refactor import RefactoringTool, get_fixers_from_package
+import re
 
 
 def FixParens(codeContents):
@@ -23,3 +24,90 @@ def FixExecStatment(codeContents):
     refactoring_tool = RefactoringTool(fixer_names=['lib2to3.fixes.fix_exec'])
     node3 = refactoring_tool.refactor_string(codeContents, 'script')
     return str(node3)
+
+
+def FixNumLiterals(codeContents):
+    refactoring_tool = RefactoringTool(fixer_names=['lib2to3.fixes.fix_numliterals'])
+    node3 = refactoring_tool.refactor_string(codeContents, 'script')
+    return str(node3)
+
+
+def FixPrintStatement(codeContents):
+    refactoring_tool = RefactoringTool(fixer_names=['lib2to3.fixes.fix_print'])
+    node3 = refactoring_tool.refactor_string(codeContents, 'script')
+    return str(node3)
+
+
+def FixExceptStatement(codeContents):
+    refactoring_tool = RefactoringTool(fixer_names=['lib2to3.fixes.fix_except'])
+    node3 = refactoring_tool.refactor_string(codeContents, 'script')
+    return str(node3)
+
+
+def FixXrange(codeContents):
+    refactoring_tool = RefactoringTool(fixer_names=['lib2to3.fixes.fix_xrange'])
+    node3 = refactoring_tool.refactor_string(codeContents, 'script')
+    return str(node3)
+
+
+def FixBasestring(codeContents):
+    refactoring_tool = RefactoringTool(fixer_names=['lib2to3.fixes.fix_basestring'])
+    node3 = refactoring_tool.refactor_string(codeContents, 'script')
+    return str(node3)
+
+
+def FixImports(codeContents):
+    refactoring_tool = RefactoringTool(fixer_names=['lib2to3.fixes.fix_imports'])
+    node3 = refactoring_tool.refactor_string(codeContents, 'script')
+    return str(node3)
+
+
+def FixDict(codeContents):
+    refactoring_tool = RefactoringTool(fixer_names=['lib2to3.fixes.fix_dict'])
+    node3 = refactoring_tool.refactor_string(codeContents, 'script')
+    return str(node3)
+
+
+def FixFunctionAttribs(codeContents):
+    refactoring_tool = RefactoringTool(fixer_names=['lib2to3.fixes.fix_funcattrs'])
+    node3 = refactoring_tool.refactor_string(codeContents, 'script')
+    return str(node3)
+
+
+def GenericReplacements(codeContents):
+    inner_code = codeContents
+    if "b\"" in inner_code:
+        inner_code = re.sub(r'os\.environ\.get\(b', "os.environ.get(", inner_code)
+    if "basestring)" in inner_code:
+        inner_code = re.sub(r"basestring\)", "str)", inner_code)
+    return inner_code
+
+
+def PerformFixes(codeContents):
+    inner_code = codeContents
+    if re.search(r"(for \w* in )", inner_code, re.IGNORECASE | re.MULTILINE):
+        inner_code = FixParens(inner_code)
+    if re.search(r"(= 0[0-9]+)", inner_code, re.IGNORECASE | re.MULTILINE):
+        inner_code = FixNumLiterals(inner_code)
+    if re.search(r"(ur\"|ur\')", inner_code, re.IGNORECASE | re.MULTILINE):
+        inner_code = FixUnicode(inner_code)
+    if "urllib" in inner_code:
+        inner_code = FixUrllib(inner_code)
+    if "unicode(" in inner_code:
+        inner_code = FixUnicode(inner_code)
+    if "exec " in inner_code:
+        inner_code = FixExecStatment(inner_code)
+    if "print " in inner_code:
+        inner_code = FixPrintStatement(inner_code)
+    if "except " in inner_code:
+        inner_code = FixExceptStatement(inner_code)
+    if "xrange" in inner_code:
+        inner_code = FixXrange(inner_code)
+    if "urlparse" in inner_code:
+        inner_code = FixImports(inner_code)
+    if "iteritems" in inner_code:
+        inner_code = FixDict(inner_code)
+    if "func_name" in inner_code:
+        inner_code = FixFunctionAttribs(inner_code)
+    inner_code = GenericReplacements(inner_code)
+    return inner_code
