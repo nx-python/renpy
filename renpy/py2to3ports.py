b22ecf73dafcc2795e9a6ac04ffd3834ec349426
noah the goodra <peterpan0413@live.com>
2018-09-24 19:21:42 -0500

    update my fixes
    Signed-off-by: noah the goodra <peterpan0413@live.com>

--- renpy/py2to3ports.py
+++ renpy/py2to3ports.py
@@ -3,8 +3,11 @@ from lib2to3.refactor import RefactoringTool as _RefactoringTool
 
 def FixParens(codeContents):
     refactoring_tool = _RefactoringTool(fixer_names=['lib2to3.fixes.fix_paren'])
-    node3 = refactoring_tool.refactor_string(codeContents, 'script')
-    return str(node3)
+    try:
+        node3 = refactoring_tool.refactor_string(codeContents, 'script')
+        return str(node3)
+    except:
+        return codeContents
 
 
 def FixUrllib(codeContents):
@@ -73,26 +76,43 @@ def FixFunctionAttribs(codeContents):
     return str(node3)
 
 
-def FixImportsRelativity(codeContents,FileName):
+def FixImportsRelativity(codeContents, FileName):
     refactoring_tool = _RefactoringTool(fixer_names=['lib2to3.fixes.fix_import'])
     node3 = refactoring_tool.refactor_string(codeContents, FileName)
     return str(node3)
+
+
+def FixRaise(codeContents):
+    refactoring_tool = _RefactoringTool(fixer_names=['lib2to3.fixes.fix_raise'])
+    node3 = refactoring_tool.refactor_string(codeContents, 'script')
+    return str(node3)
+
+
+def FixThrow(codeContents):
+    refactoring_tool = _RefactoringTool(fixer_names=['lib2to3.fixes.fix_throw'])
+    node3 = refactoring_tool.refactor_string(codeContents, 'script')
+    return str(node3)
+
 
 def GenericReplacements(codeContents):
     import re
     inner_code: str = codeContents
     if "b\"" in inner_code:
         inner_code = re.sub(r'os\.environ\.get\(b', "os.environ.get(", inner_code)
+        inner_code = re.sub(r'os.environ.pop\(b', "os.environ.pop(", inner_code)
     if "basestring)" in inner_code:
         inner_code = re.sub(r"basestring\)", "str)", inner_code)
     if " file(" in inner_code:
         inner_code = re.sub(r" file\(", " open(", inner_code)
+    if "unicode(" in inner_code:
+        inner_code = re.sub(r" unicode\("," str(", inner_code)
     return inner_code
 
 
 def PerformFixes(codeContents, Filename):
     import re
     inner_code: str = codeContents
+    inner_code = GenericReplacements(inner_code)
     if re.search(r"(for \w* in )", inner_code, re.IGNORECASE | re.MULTILINE):
         inner_code = FixParens(inner_code)
     if re.search(r"(= 0[0-9]+)", inner_code, re.IGNORECASE | re.MULTILINE):
@@ -119,5 +139,6 @@ def PerformFixes(codeContents, Filename):
         inner_code = FixDict(inner_code)
     if "func_name" in inner_code:
         inner_code = FixFunctionAttribs(inner_code)
-    inner_code = GenericReplacements(inner_code)
+    if "throw(" in inner_code:
+        inner_code = FixThrow(inner_code)
     return inner_code
